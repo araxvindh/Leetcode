dc23b4c0972d0d670e4c20788a184be41023c386
Aravind <135250289+araxvindh@users.noreply.github.com>
2025-02-11 23:23:21 +0530

    Time: 3 ms (26.06%) | Memory: 9.6 MB (98.25%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int>st;
-        st.push(-1);
+        //optimal soln without using  space
 
+        int left=0;
+        int right=0;
         int maxLen=0;
+
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(')
             {
-                st.push(i);
+                left++;
             }
             else
             {
-                st.pop();
-                if(st.empty())
-                {
-                    st.push(i);
-                }
-                else
-                {
-                    maxLen=max(maxLen , i-st.top());
-                }
+                right++;
+            }
+            if(left==right)
+            {
+                maxLen=max(maxLen,left*2);
+            }
+
+            if(right>left)
+            {
+                left=0;
+                right=0;
+            }
+        }
+
+          left=0;
+         right=0;
+
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]=='(')
+            {
+                left++;
+            }
+            else
+            {
+                right++;
+            }
+            if(left==right)
+            {
+                maxLen=max(maxLen,left*2);
+            }
+
+            if(right<left)
+            {
+                left=0;
+                right=0;
             }
         }
         return maxLen;
