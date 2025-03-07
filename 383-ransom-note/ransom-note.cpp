2e5e15fef7534919598f370b10e4942692c8a324
Aravindh <135250289+araxvindh@users.noreply.github.com>
2025-03-07 22:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (52.38%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mpp;
+        for(auto it : magazine)
+        {
+            mpp[it]++;
+        }
+
+        for(auto  it :ransomNote)
+        {
+            if(mpp[it]>0)
+            {
+                mpp[it]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
